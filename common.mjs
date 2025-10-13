1c2512b6e6992560c0e852affe6f9b5886210a4e
Rashaad Ebrahim <rmebrahim@gmail.com>
2025-10-13 22:58:19 +0200

    Created objects to convert days and months to numbers/indexes

--- common.mjs
+++ common.mjs
@@ -1,5 +1,36 @@
 // This is a placeholder file which shows how you can define functions which can be used from both a browser script and a node script. You can delete the contents of the file once you have understood how it works.
 
 export function getGreeting() {
-    return "Hello";
+  return "Hello";
 }
+
+// I was first going to create function to retrieve the index of the months and days
+// but I figured using an object would be simpler
+
+// Month name to number
+const MONTHS = {
+  January: 0,
+  February: 1,
+  March: 2,
+  April: 3,
+  May: 4,
+  June: 5,
+  July: 6,
+  August: 7,
+  September: 8,
+  October: 9,
+  November: 10,
+  December: 11,
+};
+
+// Day name to number
+const DAYS = {
+  Sunday: 0,
+  Monday: 1,
+  Tuesday: 2,
+  Wednesday: 3,
+  Thursday: 4,
+  Friday: 5,
+  Saturday: 6,
+};
+
